ee550f59d90e5bec9b7a59b6fc723ddb5d80b3e8
gsista-booth <gsista@chicagobooth.edu>
2022-11-10 06:51:50 +0000

    Photo Details looking good

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,17 @@ class PhotosController < ApplicationController
 
     render({ :template => "photo_templates/index.html.erb" })
   end
+
+  def show
+
+    #Parameters: {"path_id"=>"777"}
+
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id })
+
+    @the_photo = matching_photos.first
+
+    render({ :template => "photo_templates/show.html.erb" })
+  end
 end
