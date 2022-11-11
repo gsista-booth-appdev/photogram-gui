38934a9556ac675c3a542b0cafa8f18adc1616b8
gsista-booth <gsista@chicagobooth.edu>
2022-11-11 02:42:37 +0000

    Add User Form Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,20 @@ class UsersController < ApplicationController
     render({ :template => "user_templates/show.html.erb" })
     # end
   end
+
+  def create
+    input_username = params.fetch("query_username")
+
+    a_new_username = User.new
+    a_new_username.username = input_username
+
+    a_new_username.save
+
+    # render({ :template => "photo_templates/create.html.erb" })
+    next_url = "/users/" + a_new_username.username
+
+    redirect_to(next_url)
+
+    #render({ :template => "user_templates/create.html.erb" })
+  end
 end
